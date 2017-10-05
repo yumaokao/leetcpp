458eb957dbf552c5735428eb85056829b5e4ecc6
yumaokao <ymkq9h@gmail.com>
2017-10-06 06:09:31 +0800

    leet: accepted 34

--- answers/34/ans.cpp
+++ answers/34/ans.cpp
@@ -8,7 +8,53 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-       	vector<int> ans = {-1, -1}; 
+        vector<int> ans = {-1, -1};
+        if (nums.empty())
+            return ans;
+        if (nums.size() == 1)
+            return (nums[0] == target) ? vector<int>({0, 0}) : ans;
+
+        int i = -1;
+        int j = nums.size();
+        int k = -1;
+        while (i < j - 1) {
+            k = i + (j - i) / 2;
+            if (nums[k] == target)
+                break;
+            if (nums[k] < target)
+                i = k;
+            else
+                j = k;
+        }
+        if (nums[k] != target)
+            return ans;
+
+        // cout << " i " << i << " j " << j << " k " << k << endl;
+
+        int is = i;
+        int ks = k;
+        while (is < ks - 1) {
+            int s = is + (ks - is) / 2;
+            if (nums[s] == target)
+                ks = s;
+            else
+                is = s;
+        }
+        // cout << " ks " << ks << endl;
+        ans[0] = ks;
+
+        int ke = k;
+        int je = j;
+        while (ke < je - 1) {
+            int e = ke + (je - ke) / 2;
+            if (nums[e] == target)
+                ke = e;
+            else
+                je = e;
+        }
+        // cout << " ke " << ke << endl;
+        ans[1] = ke;
+
 		return ans;
     }
 };
@@ -25,14 +71,64 @@ int main() {
 	vector<int> nums;
 	int target;
 	vector<int> ans;
-	
-	nums = {};	
+
+	nums = {};
 	target = 5;
 	ans = s.searchRange(nums, target);
 	showAns(nums, target, ans);
 
-	nums = {5, 7, 7, 8, 8, 10};	
+	nums = {5};
+	target = 5;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5};
+	target = 3;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {3, 5};
+	target = 1;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {3, 5};
+	target = 3;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {3, 5};
+	target = 5;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
+	target = 5;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
+	target = 6;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
+	target = 7;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
 	target = 8;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
+	target = 9;
+	ans = s.searchRange(nums, target);
+	showAns(nums, target, ans);
+
+	nums = {5, 7, 7, 8, 8, 10};
+	target = 10;
 	ans = s.searchRange(nums, target);
 	showAns(nums, target, ans);
 
