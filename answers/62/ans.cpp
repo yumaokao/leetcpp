bd10345449622db83963e3b674d1b1f01c0495e7
yumaokao <ymkq9h@gmail.com>
2017-10-08 07:56:36 +0800

    leet: 62. Unique Paths accepted

---
+++ answers/62/ans.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <climits>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int cols = max(m, n);
+        int rows = min(m, n);
+        int a[cols] = { 0 };
+        for (int i = 0; i < cols; i++)
+            a[i] = 1;
+        for (int j = 1; j < rows; j++) {
+            int l = a[0];
+            for (int i = 1; i < cols; i++) {
+                a[i] += l;
+                l = a[i];
+            }
+        }
+        /* for (int i = 0; i < cols; i++)
+            cout << a[i] << endl; */
+        return a[cols - 1];
+    }
+};
+
+void showAns(int m, int n, int ans) {
+	cout << "m = " << m << ", n = " << n << " -> " << ans << " paths" << endl;
+}
+
+int main() {
+    Solution s;
+	int m, n;;
+	int ans;
+
+	m = 1;
+	n = 2;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 3;
+	n = 7;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 1;
+	n = 1;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 3;
+	n = 8;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 2;
+	n = 2;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 2;
+	n = 3;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+	m = 3;
+	n = 3;
+	ans = s.uniquePaths(m, n);
+	showAns(m, n, ans);
+
+    return 0;
+}
