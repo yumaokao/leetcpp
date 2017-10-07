26f22da5e535c2df67361c288da6c24e7bd3e0b2
yumaokao <ymkq9h@gmail.com>
2017-10-08 07:04:03 +0800

    leet: 56. Merge Intervals accepted

--- answers/56/ans.cpp
+++ answers/56/ans.cpp
@@ -27,8 +27,30 @@ struct Interval {
 
 class Solution {
 public:
+    static int istart(Interval& a) { return min(a.start, a.end); }
+    static int iend(Interval& a) { return max(a.start, a.end); }
+    static bool comp(Interval& a, Interval& b) { return (istart(a) < istart(b)); }
+
     vector<Interval> merge(vector<Interval>& intervals) {
-      	return intervals; 
+        sort(intervals.begin(), intervals.end(), comp);
+        if (intervals.size() <= 1)
+            return intervals;
+
+        vector<Interval> r;
+        Interval cur = intervals[0];
+        for (auto i: intervals) {
+            if (cur.start == i.start && cur.end == i.end)
+                continue;
+            if (istart(i) <= iend(cur)) {
+                cur.start = min(istart(cur), istart(i));
+                cur.end = max(iend(cur), iend(i));
+            } else {
+                r.push_back(cur);
+                cur = i;
+            }
+        }
+        r.push_back(cur);
+        return r;
     }
 };
 
@@ -47,7 +69,31 @@ int main() {
 	vector<Interval> intervals;
 	vector<Interval> ans;
 
-	intervals = {Interval(1, 3), Interval(2, 6)};
+	intervals = {Interval(1, 3), Interval(2, 6), Interval(8, 10), Interval(15, 18)};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {Interval(8, 10), Interval(2, 6), Interval(1, 3), Interval(15, 18)};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {Interval(1, 3), Interval(1, 3)};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {Interval(-1, 3), Interval(1, 3)};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {Interval(-1, -3), Interval(1, 3)};
+	ans = s.merge(intervals);
+	showAns(intervals, ans);
+
+	intervals = {Interval(-1, -3), Interval(1, 3), Interval(-2, -4)};
 	ans = s.merge(intervals);
 	showAns(intervals, ans);
 
