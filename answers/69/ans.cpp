d6cc26006e1a3224a2b041fd162652b336312215
yumaokao <ymkq9h@gmail.com>
2017-10-08 08:43:17 +0800

    leet: init 69. Sqrt(x)

---
+++ answers/69/ans.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <climits>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        return 0;
+    }
+};
+
+void showAns(int x, int ans) {
+	cout << "sqrt(" << x << ") = " << ans << endl;
+}
+
+int main() {
+    Solution s;
+	int x;
+	int ans;
+
+	x = 0;
+	ans = s.mySqrt(x);
+	showAns(x, ans);
+
+    return 0;
+}
