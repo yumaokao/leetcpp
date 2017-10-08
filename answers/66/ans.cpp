20e9ab3f5d584fa50bfc2da7b3adba70b201f2a6
yumaokao <ymkq9h@gmail.com>
2017-10-08 08:24:29 +0800

    leet: 66. Plus One accepted

--- answers/66/ans.cpp
+++ answers/66/ans.cpp
@@ -11,7 +11,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        return digits;
+        vector<int> r = digits;
+        bool carry = true;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            // cout << digits[i] << endl;
+            if (carry) {
+                r[i] = (digits[i] == 9) ? 0 : digits[i] + 1;
+                carry = (r[i] == 0);
+            } else
+                break;
+        }
+        if (carry)
+            r.insert(r.begin(), 1);
+        return r;
     }
 };
 
@@ -31,6 +43,26 @@ int main() {
 	vector<int> ans;
 
 	digits = {0};
+	ans = s.plusOne(digits);
+	showAns(digits, ans);
+
+	digits = {9};
+	ans = s.plusOne(digits);
+	showAns(digits, ans);
+
+	digits = {1, 9};
+	ans = s.plusOne(digits);
+	showAns(digits, ans);
+
+	digits = {9, 8};
+	ans = s.plusOne(digits);
+	showAns(digits, ans);
+
+	digits = {9, 9};
+	ans = s.plusOne(digits);
+	showAns(digits, ans);
+
+	digits = {1, 0, 0};
 	ans = s.plusOne(digits);
 	showAns(digits, ans);
 
