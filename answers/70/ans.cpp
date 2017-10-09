f7be0eba27037ca257197e4729cb6726c90cdbc8
yumaokao <ymkq9h@gmail.com>
2017-10-10 07:20:45 +0800

    leet: 70. Climbing Stairs accepted

--- answers/70/ans.cpp
+++ answers/70/ans.cpp
@@ -11,9 +11,24 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
+#if 0
         if (n < 4)
             return n;
         return climbStairs(n - 1) + climbStairs(n - 2);
+#endif
+        if (n < 3)
+            return n;
+        n -= 2;
+        int p1 = 2;
+        int p2 = 1;
+        int c = 0;
+        while (n > 0) {
+            c = p2 + p1;
+            n--;
+            p2 = p1;
+            p1 = c;
+        }
+        return c;
     }
 };
 
