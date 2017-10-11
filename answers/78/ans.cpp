c80e546a9b964da518aeaf3a5996e66645ce3947
yumaokao <ymkq9h@gmail.com>
2017-10-12 07:04:51 +0800

    leet: 78. Subsets accepted

--- answers/78/ans.cpp
+++ answers/78/ans.cpp
@@ -9,8 +9,22 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
        	vector<vector<int>> r; 
-		r.push_back({});
-		r.push_back(nums);
+        if (nums.empty())
+            return r;
+
+        int bits = 2 << nums.size() - 1;
+        for (int i = 0; i < bits; i++) {
+            vector<int> s;
+            int n = i;
+            int j = 0;
+            while (n > 0) {
+                if ((n % 2) == 1)
+                    s.push_back(nums[j]);
+                n = n >> 1;
+                j++;
+            }
+            r.push_back(s);
+        }
 		return r;
     }
 };
@@ -36,6 +50,22 @@ int main() {
 	vector<vector<int>> ans;
 
 	nums = {1, 2, 3};
+	ans = s.subsets(nums);
+	showAns(nums, ans);
+
+	nums = {};
+	ans = s.subsets(nums);
+	showAns(nums, ans);
+
+	nums = {1};
+	ans = s.subsets(nums);
+	showAns(nums, ans);
+
+	nums = {1, 2};
+	ans = s.subsets(nums);
+	showAns(nums, ans);
+
+	nums = {1, 2, 3, 4};
 	ans = s.subsets(nums);
 	showAns(nums, ans);
 
