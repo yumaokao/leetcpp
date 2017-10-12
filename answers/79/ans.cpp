f2c85ae5ef3fc0cf758b8d0a86c4e37960167f9b
yumaokao <ymkq9h@gmail.com>
2017-10-12 18:43:51 +0800

    leet: update 79

--- answers/79/ans.cpp
+++ answers/79/ans.cpp
@@ -9,7 +9,20 @@ using namespace std;
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        unordered_map<char , int> rmap;
+        if (board.empty())
+            return false;
+        if (board[0].empty())
+            return false;
+        if (word.empty())
+            return false;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                char c = word[0];
+                if (c == board[i][j]) {
+                    board[i][j] = '#';
+                }
+            }
+        }
        	return false; 
     }
 };
@@ -32,7 +45,7 @@ int main() {
 	string word;
 	bool ans;
 
-	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'};
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
 	word = "ABCCED";
 	ans = s.exist(board, word);
 	showAns(board, word, ans);
