43bd2541383e203b83e0cd43ab2e7a58e41ffedd
yumaokao <ymkq9h@gmail.com>
2017-10-13 07:21:02 +0800

    leet: 79. Word Search accepted

--- answers/79/ans.cpp
+++ answers/79/ans.cpp
@@ -8,6 +8,47 @@ using namespace std;
 
 class Solution {
 public:
+    bool isExist(vector<vector<char>>& board, string word, int i, int j) {
+        if (word.empty())
+            return true;
+        char c = word[0];
+        // cout << " i " << i << " j " << j << " word " << word << " c " << c<< endl;
+        if (i > 0) {  // up
+            if (c == board[i - 1][j]) {
+                vector<vector<char>> nb = board;
+                nb[i - 1][j] = '#';
+                if (isExist(nb, word.substr(1, word.size() - 1), i - 1, j))
+                    return true;
+            }
+        }
+        if (i < board.size() - 1) {  // down
+            if (c == board[i + 1][j]) {
+                vector<vector<char>> nb = board;
+                nb[i + 1][j] = '#';
+                if (isExist(nb, word.substr(1, word.size() - 1), i + 1, j))
+                    return true;
+            }
+        }
+        if (j > 0) {  // left
+            if (c == board[i][j - 1]) {
+                vector<vector<char>> nb = board;
+                nb[i][j - 1] = '#';
+                if (isExist(nb, word.substr(1, word.size() - 1), i, j - 1))
+                    return true;
+            }
+        }
+        if (j < board[0].size() - 1) {  // right
+            if (c == board[i][j + 1]) {
+                vector<vector<char>> nb = board;
+                nb[i][j + 1] = '#';
+                if (isExist(nb, word.substr(1, word.size() - 1), i, j + 1))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
     bool exist(vector<vector<char>>& board, string word) {
         if (board.empty())
             return false;
@@ -19,7 +60,10 @@ public:
             for (int j = 0; j < board[0].size(); j++) {
                 char c = word[0];
                 if (c == board[i][j]) {
-                    board[i][j] = '#';
+                    vector<vector<char>> nb = board;
+                    nb[i][j] = '#';
+                    if (isExist(nb, word.substr(1, word.size() - 1), i, j))
+                        return true;
                 }
             }
         }
@@ -47,6 +91,21 @@ int main() {
 
 	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
 	word = "ABCCED";
+	ans = s.exist(board, word);
+	showAns(board, word, ans);
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+	word = "SEE";
+	ans = s.exist(board, word);
+	showAns(board, word, ans);
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+	word = "ABCB";
+	ans = s.exist(board, word);
+	showAns(board, word, ans);
+
+	board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+	word = "ABCCEF";
 	ans = s.exist(board, word);
 	showAns(board, word, ans);
 
