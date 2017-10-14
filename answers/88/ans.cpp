34a38db79566905a546528b7aa169dcd38160a4c
yumaokao <ymkq9h@gmail.com>
2017-10-14 10:35:24 +0800

    leet: 88. Merge Sorted Array accepted

--- answers/88/ans.cpp
+++ answers/88/ans.cpp
@@ -8,6 +8,25 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // cout << " nums1.size() " << nums1.size() << endl;
+        nums1.resize(m + n);
+        // cout << " nums1.size() " << nums1.size() << endl;
+        int i = m - 1;
+        int j = n - 1;
+        while (i > -1 || j > -1) {
+            if (j == -1)
+                break;
+            int n1 = (i > -1) ? nums1[i] : 0;
+            int n2 = (j > -1) ? nums2[j] : 0;
+            // cout << " i " << i << " n1 " << n1 << " j " << j << " n2 " << n2 << endl;
+            if (n1 < n2 || i == -1) {
+                nums1[i + j + 1] = n2;
+                j--;
+            } else {
+                nums1[i + j + 1] = n1;
+                i--;
+            }
+        }
 		return;        
     }
 };
@@ -24,8 +43,62 @@ int main() {
 	vector<int> nums1;
 	vector<int> nums2;
 
+	nums1 = {1, 3, 5};
+	nums2 = {2, 4, 6};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
 	nums1 = {1, 2, 3};
 	nums2 = {4, 5, 6};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
+	nums1 = {};
+	nums2 = {4, 5, 6};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
+	nums1 = {1, 2, 3};
+	nums2 = {};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
+	nums1 = {};
+	nums2 = {};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
+	nums1 = {1};
+	nums2 = {};
+	showNums(nums1);
+	showNums(nums2);
+	cout << " -> " << endl;
+	s.merge(nums1, nums1.size(), nums2, nums2.size());
+	showNums(nums1);
+	cout << endl;
+
+	nums1 = {-1, 0, 0, 3, 3, 3};
+	nums2 = {1, 2, 2};
 	showNums(nums1);
 	showNums(nums2);
 	cout << " -> " << endl;
