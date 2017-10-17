fe297ab22a9266696a0bc5b50dc8a4d101befad9
yumaokao <ymkq9h@gmail.com>
2017-10-17 14:33:23 +0800

    leet: 91. Decode Ways accepted

--- answers/91/ans.cpp
+++ answers/91/ans.cpp
@@ -5,6 +5,47 @@
 
 using namespace std;
 
+//      1, 1, 1,
+//-> 1  1, 1+1
+//      1, 1, 1,
+//->    1, 2, 1+2
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty())
+            return 0;
+        if (s.size() == 1)
+            return (s[0] == '0') ? 0 : 1;
+        if (s[0] == '0')
+            return 0;
+        int n1 = 1;
+        int n2 = 1;
+        for (int i = 1; i < s.size(); i++) {
+            char c = s[i];
+            char p = s[i - 1];
+            int n = 0;
+            if (c == '0' && p == '0')
+                return 0;
+            if (c == '0') {
+                if (p == '1' || p == '2')
+                    n = n2;
+                else
+                    return 0;
+            } else {
+                if (p == '1' || (p == '2' && (c > '0' && c < '7'))) {
+                    n = n1 + n2;
+                } else
+                    n = n1;
+            }
+
+            n2 = n1;
+            n1 = n;
+        }
+        return n1;
+    }
+};
+
+#if 0
 class Solution {
 public:
     int numDecodings(string s) {
@@ -43,6 +84,7 @@ public:
         return numDecodingTwoChars(s.substr(1, s.size() - 1));
     }
 };
+#endif
 
 void showAns(string s, int ans) {
 	cout << "[" << s << "] -> " << ans << endl;
@@ -90,6 +132,14 @@ int main() {
 	showAns(s, ans);
 
 	s = "100";
+	ans = sol.numDecodings(s);
+	showAns(s, ans);
+
+	s = "27";
+	ans = sol.numDecodings(s);
+	showAns(s, ans);
+
+	s = "01";
 	ans = sol.numDecodings(s);
 	showAns(s, ans);
 
